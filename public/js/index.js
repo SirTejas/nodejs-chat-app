e85ca1a2641390192042b15193601cbe889c81d3
Tejas <sirtejas.jaiswal@gmail.com>
2020-01-26 15:38:15 +0530

    Add mustache to render message-template and Add scrollToBottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 const socket = io();
 
+function scrollToBottom() {
+    //selector
+    let messages = jQuery('#messages');
+    let newMessage = messages.children('li:last-child');
+
+    //heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
 
 socket.on('connect',()=>{
     console.log('connected to server');
@@ -10,20 +26,30 @@ socket.on('connect',()=>{
 })
 
 socket.on('newMessage',(data)=>{
-    console.log('Clients newMessage event listener :');
-    console.log(data);
 
-    let li = jQuery('<li></li>');
-    li.text(`${data.from} ${data.createdAt} : ${data.text}`);
-    jQuery('#messages').append(li);
+    let template = jQuery('#message-template').html();
+    let html = Mustache.render(template,{
+        from : data.from,
+        text : data.text,
+        createdAt : data.createdAt
+    });
 
+    jQuery('#messages').append(html);
+    scrollToBottom();
 
+    // console.log('Clients newMessage event listener :');
+    // console.log(data);
+
+    // let li = jQuery('<li></li>');
+    // li.text(`${data.from} ${data.createdAt} : ${data.text}`);
+    // jQuery('#messages').append(li);
 });
 
 socket.on('userConnected',(data) =>{
     let li = jQuery('<li></li>');
     li.text(`${data.from} ${data.createdAt} : ${data.text}`);
     jQuery('#messages').append(li);
+    scrollToBottom();
 });
 
 socket.on('userDisconnected',(data)=>{
@@ -31,6 +57,7 @@ socket.on('userDisconnected',(data)=>{
 
     li.text(`${data.from} ${data.createdAt} : ${data.text}`);
     jQuery('#messages').append(li);
+    scrollToBottom();
 })
 
 
